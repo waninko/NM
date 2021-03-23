16357106891df84fb8360953265a82e14e40c8f5
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-03-23 10:34:02 +0100

    sds

--- oblig/MVCDIVS.js
+++ oblig/MVCDIVS.js
@@ -3,70 +3,71 @@
         let html = ``;
         //tenge opp stats utifra anvn
         //mellomlagre id osv i hver sin - dollare ut i viewloop
-      for(let option of Ninjas){ //gir muligheten til og dra ut single string av en flere objektet arrey 
+      for(let option of Ninjas){ //gir muligheten til og dra ut single string av en flere objektet arrey
             if(option.NinjaName == selectedNinja){
-                html += `<div Class=Cost>Cost</div>`
+                 html += `<div Class=Cost>Cost</div>`
                 html += `<div Class=chakra>${option.Chakra}</div>`
-                html += `<div Class=quarter1>can be quartered?: </div>`
-                html += `<div Class=NY> ${option.Forbidden}</div>` 
-                html += `<div Class=quarter2>can be quartered?: </div>`
-                html += `<div Class=YN>${option.FixedChakra}</div>`
-                html += `<div Class=qq>Stats+Used</div>`
-                html += `<div Class=used> number used </div>`
-               
                 
+                html += `<div Class=quarter1>can be quartered?: </div>`
+                 html += `<div Class=NY> ${option.Forbidden}</div>`
+                 html += `<div Class=quarter2>can be quartered?: </div>`
+                 html += `<div Class=YN>${option.FixedChakra}</div>`
+                html += `<div Class=qq>Stats+Used</div>`
+                html += `<div Class=used> auto input </div>`
+
+
           html += `</div>`;
-                html += `<div class=Statpoints-Point>Statspoint `;
-                html += `<div Class=s01></div>`
-                html += `<div Class=s02>in</div>`
-                html += `<div Class=s03>in</div>`
-                html += `<div Class=s04>in</div>`
-                html += `<div Class=s05>in</div>`
-                html += `<div Class=s06>in</div>`
-                html += `<div Class=s07>in</div>`
-                html += `<div Class=s08>in</div>`
-                html += `<div Class=s09>in</div>`
-                html += `<div Class=s10>in</div>`
-                html += `<div Class=s11>in</div>`
-                html += `<div Class=s12>in</div>`
-                html += `<div Class=s13>in</div>`
-                html += `<div Class=s14>in</div>`
-                html += `<div Class=s15>in</div>` 
-                html += `<div Class=s16>in</div>`
-                html += `<div Class=s17>in</div>`
-                html += `<div Class=s18>in</div>`
-                html += `<div Class=s19>in</div>`
-                html += `<div Class=s20>in</div>`
+                html += `<div class=Statpoints-Point><b>Statspoint</b></div> `;
+                html += `<div Class=s01>tai in</div>`
+                html += `<div Class=s02> nin in</div>`
+                html += `<div Class=s03>buki in</div>`
+                html += `<div Class=s04>stam in</div>`
+                html += `<div Class=s05>ele in</div>`
+                html += `<div Class=s06> gen in in</div>`
+                html += `<div Class=s07> atack</div>`
+                html += `<div Class=s08>Strike in</div>`
+                html += `<div Class=s09> hit in</div>`
+                html += `<div Class=s10> Roll in</div>`
+                html += `<div Class=s11> focus in</div>`
+                html += `<div Class=s12> Burst in</div>`
+                html += `<div Class=s13> Range in</div>`
+                html += `<div Class=s14>  booost in</div>`
+                html += `<div Class=s15> Recovery in</div>`
+                html += `<div Class=s16> gen act in</div>`
+                html += `<div Class=s17> gen mast in</div>`
+                html += `<div Class=s18> gen rec in</div>`
+                // html += `<div Class=s19>in</div>`
+                // html += `<div Class=s20>in</div>`
                 // html += `<div Class=s21>in</div>`
                 // html += `<div Class=s22>in</div>`
                 html += `</div>`;
           html += `</div>`;
-               
-                // html += `<div id="selectResult">`;
-                // html += `<div Class=avrdmg>Avrage dmg :in</div>`
-                // html += `<div Class=D-R>Damage range :in</div>`
-                // html += `<div Class=avrdmgcritt>Avrage dmg on critt :in</div>`
-                // html += `<div Class=Dmgran>Dmg range :in</div>`
-                // html += `<div Class=low-dmg>low dmg in</div>`
-                // html += `<div Class=high-dmg>high dmg in</div>`
-                // html += `<div Class=avrcrit-dmg>avrage critt in</div>`
-                // html += `<div Class=dmg-rng-critt> dmg range critt in</div>`
-                // html += `<div Class=low-critt>low critt in</div>`
-                // html += `<div Class=high-critt>high critt inn</div>`
-                
+
+                html += `<div id="selectResult">`;
+                html += `<div Class=avrdmg>Avrage dmg :in</div>`
+                html += `<div Class=D-R>Damage range :in</div>`
+                html += `<div Class=avrdmgcritt>Avrage dmg on critt :in</div>`
+                html += `<div Class=Dmgran>Dmg range :in</div>`
+                html += `<div Class=low-dmg>low dmg in</div>`
+                html += `<div Class=high-dmg>high dmg in</div>`
+                html += `<div Class=avrcrit-dmg>avrage critt in</div>`
+                html += `<div Class=dmg-rng-critt> dmg range critt in</div>`
+                html += `<div Class=low-critt>low critt in</div>`
+                html += `<div Class=high-critt>high critt inn</div>`
+
 
                 html += `<div class=position>Position: ${option.Position}</div>`
                 html += `<div class=seal>Seal: ${option.Seal}</div>`
                 html += `</div>`;
-                html += `<div id="txt">`;
                 html += `<div class=The-Stats>STATS</div>`;
+                html += `<div id="txt">`;
                 html += `<div class=Tai>Taijutsu</div>`
                 html += `<div class=Nin>Ninjutsu</div>`
                 html += `<div class=Buki>Bukijutsu</div>`
                 html += `<div class=Sta>Stamina</div>`
                 html += `<div class=Ele>Elemental</div>`
                 html += `<div class=Gen>Genjutsu</div>`
-                html += `<div class=Atk>Attack</div>`
+                html += `<div class=atk>Attack</div>`
                 html += `<div class=Strike>CriticalStrike</div>`
                 html += `<div class=Hit>Critical Hit</div>`
                 html += `<div class=roll>Reroll</div>`
@@ -78,65 +79,57 @@
                 html += `<div class=Act>Genjutsu Activation</div>`
                 html += `<div class=Mastery>Genjutsu Mastery</div>`
                 html += `<div class=Rech>Genjutsu Recharge</div>`
-                html += `<div class=Endu>Endurance</div>`
-                // html += `<div class=></div>`
+                // html += `<div class=Endu>Endurance</div>`
+                // // html += `<div class=></div>`
                 // html += `<div class=></div>`
                 // html += `<div class=></div>`
                 // html += `<div class=></div>`
                 html += `</div>`;
+                html += `<div class=bloodline>Blood line </div>`
+                html += `<div class=bloodlinelvl>Blood line LVL </div>`
+                html += `<div class=Blood-line>drop down off BL  </div>`
+                html += `<div class=Blood-Line-LvL> input BL lvl </div>`
                 //loope ut stats
-                let currentStat = ""; // mellomlagringsvar 
+                let currentStat = ""; // mellomlagringsvar
                 for(stat in option.Stats){
                     //get key
                 //     currentStat = stat; // - ta vare på "current stat" fra løkka
                 //     html += `<div>${currentStat}: ${option.Stats[stat]}</div>`;
-                // }                   //  name       stat value 
-                // let currentGrowth = "";  
-                currentStat = stat; 
+                // }                   //  name       stat value
+                // let currentGrowth = "";
+                currentStat = stat;
 
                     html += `<div class="StatNames">${option.Stats[stat]} </div>`;
-                  
-                }      
+
+                }
                 html += `</div>`;
                 html += `</div>`;
-                html += `</div>`;  
-                html += `</div>`;  
-                html += `<div id="Secondary">`;        
+                html += `</div>`;
+                html += `</div>`;
+                html += `<div id="Secondary">`;
                 let currentSecondaryStats = "";
-            
+
                 //for (Object in SecondaryStats){
-                    
+
                     currentSecondaryStats = SecondaryStats;
-                    
-                html += `<div>${SecondaryStats.Attack}</div>`;
-                html += `<div>${SecondaryStats.CriticalStrike}</div>`;
-                html += `<div>${SecondaryStats.CriticalChance} </div>`;
-                html += `<div>${SecondaryStats.Reroll}</div>`;
-                html += `<div>${SecondaryStats.FocusBurst}</div>`;
-                html += `<div>${SecondaryStats.FocusRange}</div>`;
-                html += `<div>${SecondaryStats.BukijutsuBoost}</div>`;
-                html += `<div>${SecondaryStats.BukijutsuRecovery}</div>`;  
-                html += `<div>${SecondaryStats.GenjutsuActivation}</div>`;  
-                html += `<div>${SecondaryStats.GenjutsuMastery}</div>`;  
-                html += `<div> ${SecondaryStats.GenjutsuRecharge}</div>`;  
-                html += `<div>${SecondaryStats.Endurance}</div>`;  
-                html += `<div>${SecondaryStats.FatigueRecovery}</div>`;  
-                html += `<div>${SecondaryStats. Offposition}</div>`;
-                html += `<div>${SecondaryStats.TaijutsuImmunity}</div>`;
-                html += `<div>${SecondaryStats.NinjutsuImmunity}</div>`;
-                html += `<div>${SecondaryStats.BukijutsuImmunity}</div>`;
-                html += `<div>${SecondaryStats.poison}</div>`;
-                html += `<div>${SecondaryStats.Guard}</div>`;
-                html += `<div>${SecondaryStats.Absorb}</div>`;
-                html += `<div>${SecondaryStats.Death}</div>`;
-                html += `<div>${SecondaryStats. BloodlineNullify}</div>`;
-                html += `<div>${SecondaryStats.ItemFind}</div>`;
-                html += `<div>${SecondaryStats.Gold}</div>`;
-                html += `<div>${SecondaryStats.Experience}</div>`;
-                html += `</div>`;  
+
+                html += `<div Class= Atk>${SecondaryStats.Attack}</div>`;
+                html += `<div Class= Strike>${SecondaryStats.CriticalStrike}</div>`;
+                html += `<div Class= hit>${SecondaryStats.CriticalChance} </div>`;
+                html += `<div Class= roll>${SecondaryStats.Reroll}</div>`;
+                html += `<div Class= Focus>${SecondaryStats.Focus}</div>`;
+                html += `<div Class= Burst>${SecondaryStats.FocusBurst}</div>`;
+                html += `<div Class= Range>${SecondaryStats.FocusRange}</div>`;
+                html += `<div Class= Boost>${SecondaryStats.BukijutsuBoost}</div>`;
+                html += `<div Class= Rec>${SecondaryStats.BukijutsuRecovery}</div>`;
+                html += `<div Class= Act>${SecondaryStats.GenjutsuActivation}</div>`;
+                html += `<div Class= Mastery>${SecondaryStats.GenjutsuMastery}</div>`;
+                html += `<div Class= Rech> ${SecondaryStats.GenjutsuRecharge}</div>`;
+
+                html += `</div>`;
 
                 //}
-            html+= `<div>Identifier ${option.Identifier}</div>`
+            html+= `<div Class=ID>Identifier ${option.Identifier}</div>`
             html+= `<div class= NinjaName > ${option.NinjaName}</div>`
             html+= `<div class=lvl>LVL</div>`
             let currentAbility = "";
@@ -144,20 +137,42 @@
         //         currentAbility =Ability;
         //     html+= `<div>Ability ${option.Ability[Ability]}</div>`
         // }
-         html+=`<div>fixed Chakra?: ${option.FixedChakra}</div>`
-         
-        
-       
-        
+         html+=`<div class=fixed>fixed Chakra?: ${option.FixedChakra}</div>`
+
+
+
+
          html += `</div>`;
-                
+
         html+=`<input type="text" id="LVL" /><br> <input class="button" type="button" onClick="multiplyBy()" Value="LVL" />`
-          html+=`<div ID="rsesult"> </div>` 
-        
+          html+=`<div ID="rsesult"> </div>`
+
+          html += `<div ID="Effi">` 
+          html+= `<div class="Efficantcy"><b>Efficantcy</b></div>`
+          html+= `<div class="tai-effi">tai % </div>`
+          html+= `<div class="nin-effi">nin effi% </div>`
+          html+= `<div class="buki-effi">buki effi %</div>`
+          html+= `<div class="Stam-effi">stamina effi %</div>`
+          html+= `<div class="ele-effi">ele effi %</div>`
+          html+= `<div class="gen-effi">Gen effi%</div>`
+          html+= `<div class= "atk-effi">N/A</div>`
+          html+= `<div class="strike-effi">strike %</div>`
+          html+= `<div class="hit-effi">hit %</div>`
+          html+= `<div class="reroll-effi"> reroll %</div>`
+          html+= `<div class="focus-effi"> focus %</div>`
+          html+= `<div class="focus-burst"> burst %</div>`
+          html+= `<div class="focus-range"> range %</div>`
+          html+= `<div class="boost-effi"> boost %</div>`
+          html+= `<div class="buki-rec-effi"> recov %</div>`
+          html+= `<div class="gen-act-effi"> gen act %</div>`
+          html+= `<div class="gen-mastery-effi"> mastery %</div>`
+          html+= `<div class="gen-rec-effi"> gen reac %</div>`
          
-          html += `</div>`;
-        selectResult.innerHTML =  html;          
-       
+          
+
+        //   html += `</div>`;
+        selectResult.innerHTML =  html;
+
     }
     }
 
