7821272da536229f0cdee236ae499ab386d16e08
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-03-26 10:52:43 +0100

    q

---
+++ .vscode/Items.js
@@ -0,0 +1,26 @@
+
+
+
+        {
+          {  Item : "Scroll of Sutra",
+          "Sta": 15,
+         "Buki": 40,
+          "FocusBurst":  20,
+          "FokusRange": 30,
+          "WindDamage": 25,
+          "BukijutsuBoost": -10, 
+          file:'https://github.com/devapath89/NM/blob/main/oblig/Img/Scroll%20of%20sutra.png?raw=true'
+         
+         }
+           
+           
+           { item: "Dracolich's Phylactery"
+           "Nin": 30,
+           "Gen": 70,
+           "Ele": 40,
+           "GenjutsuActivation": 30,
+           "Death": 20,
+            "Bloodline Exp": 1,
+            file:'https://github.com/devapath89/NM/blob/main/oblig/Img/Dracolich'sPhylatery.png?raw=true'
+        }
+        }
