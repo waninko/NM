56294dfda0f70877a606797c49f181c53b3fef82
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-03-27 21:53:10 +0100

    s

--- oblig/Equipment.js
+++ oblig/Equipment.js
@@ -1,26 +1,72 @@
 
-const Equipment = {
-    "Ability": [
-        "light-fang",{
-            Attack : 5,
-             link:''},
-        "shadow-thunder-blast",{
-            Nin: 10,
-            Attack : 5,
+
+
+
+
+
+
+const Equipments = {
+"Ability": [
+    "light-fang",{
+        Attack : 5,
+        link:''},
+    "shadow-thunder-blast",{
+        Nin: 10,
+        Attack : 5,
+        link:''},
+    "limbo",{
+        Tai:10,
+        Nin: 10,
+         Attack : 5,
+        link:''},
+    "divine-genesis",{
+        Nin: 30,
+        Gen: 30,
+        link:''},
+        "hair-camouflage",{
+             Nin: 11,
             link:''},
-        "limbo",{
-            Tai:10,
-            Nin: 10,
-            Attack : 5,
+        "flower-petal-escape",{
+            Nin: 15,
+            Gen: 20,
             link:''},
-        "divine-genesis",{
-            
-            Nin: 30,
-            Gen: 30,
-            link:'',}
+        "evil-sealing",{
+            Nin:15,
+            Gen:20,
+            GenjutsuActivation:10,
+            link:''},   
+                
+        "string-bean-binding",{
+            Nin:10,
+            Gen:20,
+            GenjutsuMastery:20,
+            link:''},
+            "paper-shuriken",{
+                Nin:4,
+                Buki:8,
+                link:''},
+            "paper-chakram",{
+                Nin:6,
+                Buki:10,
+                BukijutsuBoost:4,
+                link:'https://github.com/devapath89/NM/blob/main/oblig/Img/paperChakram.png?raw=true'},
+            "paper-clone",{
+                Nin:16,
+                Buki:15,
+                Sta:8,
+                link:''},
+            "sacred-paper-emissary",{
+                Nin:10,
+                Buki:10,
+                Sta:10,
+                Ele:5,
+                Reroll:10,
+                link:'https://github.com/devapath89/NM/blob/main/oblig/Img/sacred%20Paper%20emissary.png?raw=true'}
         ]
+       
+           
+         
         
                 
 }
 
-    
--- oblig/Itemview.js
+++ oblig/Itemview.js
@@ -10,9 +10,17 @@ function updateViewItem(){
     document.getElementById('hovedSide').innerHTML = '' ;
     document.getElementById('selectResult').innerHTML ='';
     let html = '';
+//     function makeDivs(){
+//        let html = ``;
+//        //tenge opp stats utifra anvn
+//        //mellomlagre id osv i hver sin - dollare ut i viewloop
+//      for(let option of Ninjas){ //gir muligheten til og dra ut single string av en flere objektet arrey
+//            if(option.NinjaName == selectedNinja){}
+
+    
     html += `<div id="Equipments"`
     html +=`<div class="Equip">Item</div>`
-    html +=`<div class="Equip">${Object.keys(Equipment.Ability): ${Object.values(Equipment.Ability)[i]}}</div>`
+    html +=`<div class="Equip">${Equipments.Ability}</div>`
            html +=`<div class="Equip"></div>`
            html +=`<div class="Equip">Item</div>`
            html +=`<div class="Equip">item</div>`
@@ -25,21 +33,17 @@ function updateViewItem(){
 
 
 
-
-
-
-
-
+hovedSide.innerHTML = html;
     document.getElementById('hovedSide').innerHTML = html;
     
     // html= `<select onchange="selectEquipment(this.value)">` 
     
    
    
-  
+   
+     }
 
 
- }
 
 
 //  makeDivs();/*send you back to main*/ 
@@ -53,5 +57,4 @@ function updateViewItem(){
 //     function runViews() {
 // MakeDropDown()
 // selectNinja()
-// makeDivs()
-// }
+// makeDivs(){} 
